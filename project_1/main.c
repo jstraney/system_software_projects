e1f9c9101ff20decb3a1c544578f0fe1e41169fa
Jeff Straney <straneyjeff@gmail.com>
2017-09-25 18:06:20 -0400

    finished load_instructions_from_file. mostly finished fetch_instruction

--- project_1/main.c
+++ project_1/main.c
@@ -1,27 +1,98 @@
-#include <stdio.h>
+/*
+______      ___  ___  ___  _____  _   _ _____ _   _  _____ 
+| ___ \     |  \/  | / _ \/  __ \| | | |_   _| \ | ||  ___|
+| |_/ /_____| .  . |/ /_\ \ /  \/| |_| | | | |  \| || |__  
+|  __/______| |\/| ||  _  | |    |  _  | | | | . ` ||  __| 
+| |         | |  | || | | | \__/\| | | |_| |_| |\  || |___ 
+\_|         \_|  |_/\_| |_/\____/\_| |_/\___/\_| \_/\____/ 
+
+*/
+// Written by Jeffrey Straney and Frank __last_name__
+// 09-22-2017
+// COP3402 assignment 1
+
+//// Includes ////
+////////////////////////////////////////////////////////////////////////////////
+#include <stdlib.h>
 #include <string.h>
 
+//// Header Files ////
+////////////////////////////////////////////////////////////////////////////////
 #ifndef MAIN_FUNCTIONS
 #include "main.h"
 #define MAIN_FUNCTIONS 1
 #endif
 
+//// Constants ////
+////////////////////////////////////////////////////////////////////////////////
 #define MAX_STACK_HEIGHT  2000
 #define MAX_CODE_LENGTH  500
+#define REG_FILE_SIZE 16 
 #define MAX_LEXI_LEVEL  3
+#define INSTRUCTION_REGISTER_SIZE 16
 
-// VM statuses
+// initial pointer values
+#define SP 0 
+#define BP 1 
+#define PC 0 
+#define IR 0 
+
+//// VM Statuses ////
+////////////////////////////////////////////////////////////////////////////////
 #define STATUS_OK  0
 #define STATUS_ERR  1
 #define STATUS_QUIT  2 
 #define STATUS_FINISHED  2 
 
-// user commands supplied running with -i flag
+//// User Commands ////
+////////////////////////////////////////////////////////////////////////////////
 #define COMMAND_INVALID  -1
 #define COMMAND_QUIT  0 
 #define COMMAND_HELP  1 
 #define COMMAND_STEP  2 
 #define COMMAND_RUN  3 
+
+//// OP Codes ////
+////////////////////////////////////////////////////////////////////////////////
+#define OP_LIT 1
+#define OP_RTN 2
+#define OP_LOD 3
+#define OP_STO 4
+#define OP_CAL 5
+#define OP_INC 6
+#define OP_JMP 7
+#define OP_JPC 8
+#define OP_SIO 9
+#define OP_NEG 10
+#define OP_ADD 11
+#define OP_SUB 12
+#define OP_MUL 13
+#define OP_DIV 14
+#define OP_ODD 15
+#define OP_MOD 16
+#define OP_EQL 17
+#define OP_NEQ 18
+#define OP_LSS 19
+#define OP_LEQ 20
+#define OP_GTR 21
+#define OP_GEQ 22
+
+//// Program Structures ////
+////////////////////////////////////////////////////////////////////////////////
+// create stack. initialize to 0
+int stack_pointer = SP;
+int stack[MAX_STACK_HEIGHT] = {0};
+
+// I am under the assumption that 'code length' refers to the number of
+// instructions
+Instruction code[MAX_CODE_LENGTH];
+
+// initialize to 0
+int program_counter = PC;
+
+// create register file. initialize to 0
+int reg[REG_FILE_SIZE] = {0};
+
 
 int main (int argsc, char *argv[]) {
 
@@ -31,9 +102,12 @@ int main (int argsc, char *argv[]) {
     interactive_loop();
 
   }
-  else {
+  else if (argsc == 2) {
 
-    event_loop();
+    // file containing instructions
+    char *filename = argv[1];
+
+    event_loop(filename);
 
   }
 
@@ -105,14 +179,23 @@ int get_user_command () {
 }
 
 // called when no arguments are provided to the binary
-int event_loop () {
+int event_loop (char *filename) {
+
+  FILE *file = fopen(filename, "r");
+
+  read_instructions_from_file(file);
+
+  fclose(file);
 
   // status okay
   int status = STATUS_OK;
 
   while (status == STATUS_OK) {
 
-    vm_procedures();
+    // load instruction from 'code'
+    Instruction instruction = fetch_instruction();
+
+    execute_instruction(instruction); 
 
   }
 
@@ -174,9 +257,215 @@ int interactive_loop () {
 
 }
 
-// the virtual machine execution cycle 
-int vm_procedures () {
+// fetches the instructions into 'code' register
+int read_instructions_from_file(FILE *file) {
 
-  return STATUS_FINISHED;
+  // locally used for inserting codes into the code register. 
+  int code_index = 0;
+
+  while (!feof(file)) {
+
+    // allocate memory for instruction
+    Instruction *instruction = malloc(sizeof(Instruction));
+
+    // go through each part of the instruction on this line
+    for (int i = 0; i < 4; i++) {
+
+      int instruction_part;
+
+      // get the next integer from the file
+      fscanf(file, "%d", &instruction_part);
+
+      // opcode, should be first of every 4 values
+      if (i % 4 == 0) {
+
+        instruction->op = instruction_part;
+
+      }
+      // register should be second
+      else if (i % 4 == 1) {
+
+        instruction->r = instruction_part;
+
+      }
+      // l should be third
+      else if (i % 4 == 2) {
+
+        instruction->l = instruction_part;
+
+      }
+      // m should be fourth
+      else if (i % 4 == 3) {
+
+        instruction->m = instruction_part;
+
+      }
+      
+    }
+
+    // place the instruction in our code register
+    code[code_index++] = *instruction;
+
+    // free allocated memory for instruction
+    free(instruction);
+
+  }
+
+  // everything okay so far
+  return 1;
+
+}
+
+Instruction fetch_instruction () {
+
+  Instruction instruction;
+
+  instruction = code[PC];
+
+  return instruction;
+
+}
+
+int execute_instruction (Instruction instruction) {
+
+  int op = instruction.op;  
+  int r  = instruction.r;
+  int l  = instruction.l;
+  int m  = instruction.m;
+
+  if (op == OP_LIT) {
+
+    reg[r] = m;
+
+  }
+
+  else if (op == OP_RTN) {
+
+  }
+
+  else if (op == OP_LOD) {
+
+  }
+  else if (op == OP_STO) {
+
+  }
+
+  else if (op == OP_CAL) {
+
+  }
+
+  else if (op == OP_INC) {
+
+    stack_pointer += m;
+
+  }
+
+  else if (op == OP_JMP) {
+
+    program_counter = m;
+
+  }
+
+  else if (op == OP_JPC) {
+
+    if (reg[r] == 0) {
+
+      program_counter = m;
+
+    }
+
+  }
+
+  else if (op == OP_SIO) {
+
+    // do multiple levels of decoding. check r, l and m
+
+  }
+
+  else if (op == OP_NEG) {
+
+    reg[r] = -(reg[l]);
+
+  }
+
+  else if (op == OP_ADD) {
+
+    reg[r] = reg[l] + reg[m];
+
+  }
+
+  else if (op == OP_SUB) {
+
+    reg[r] = reg[l] - reg[m];
+
+  }
+
+  else if (op == OP_MUL) {
+
+    reg[r] = reg[l] * reg[m];
+
+  }
+
+  else if (op == OP_DIV) {
+
+    reg[r] = reg[l] / reg[m];
+
+  }
+
+  else if (op == OP_ODD) {
+
+    reg[r] = reg[l] % 2;
+
+  }
+
+  else if (op == OP_MOD) {
+
+    reg[r] = reg[l] % reg[m];
+
+  }
+
+  else if (op == OP_EQL) {
+
+    reg[r] = reg[l] == reg[m];
+
+  }
+
+  else if (op == OP_NEQ) {
+
+    reg[r] = reg[l] != reg[m];
+
+  }
+
+  else if (op == OP_LSS) {
+
+    reg[r] = reg[l] < reg[m];
+
+  }
+
+  else if (op == OP_LEQ) {
+
+    reg[r] = reg[l] <= reg[m];
+
+  }
+
+  else if (op == OP_GTR) {
+
+    reg[r] = reg[l] > reg[m];
+
+  }
+
+  else if (op == OP_GEQ) {
+
+    reg[r] = reg[l] >= reg[m];
+
+  }
+
+  else {
+
+    return STATUS_ERR;
+
+  }
+
+  return STATUS_OK;
 
 }
--- project_1/main.h
+++ project_1/main.h
@@ -1,5 +1,34 @@
+#ifndef IO
+#include <stdio.h>
+#endif
+//// Structs ////
+typedef struct Instruction {
+  int op;
+  int r;
+  int l;
+  int m;
+} Instruction;
+
+// prompt for interactive mode
 int print_prompt();
+
+// fetches character from input, returns integer flag
 int get_user_command();
+
+// opens file with instruction code
+int open_file();
+
+// calls vm proceedures
 int event_loop();
+
+// calls vm procedures in an interactive context
 int interactive_loop();
-int vm_procedures();
+
+// read instructions from file and load into code register
+int read_instructions_from_file(FILE *file);
+
+// first stage of vm
+Instruction fetch_instruction();
+
+// branches into various instructions
+int execute_instruction(Instruction instruction);
